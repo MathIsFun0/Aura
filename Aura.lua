e4b6e2f895d64514011237139f9a4065dc04b2c5
lmesh91 <89877664+MathIsFun0@users.noreply.github.com>
2024-06-14 23:05:46 -0400

    0.9.8 fixes

--- Aura.lua
+++ Aura.lua
@@ -382,3 +382,81 @@ function Game:start_run(args)
     gsr(self,args)
     reset_castle_card(true)
 end
+
+--There's a bug with this function in 0.9.8 that screws up the joker pool
+--So we just override it to fix our problems
+if SMODS.injectJokers then
+    function SMODS.injectJokers()
+        local minId = table_length(G.P_CENTER_POOLS['Joker']) + 1
+        local id = 0
+        local i = 0
+        local joker = nil
+        for k, slug in ipairs(SMODS.BUFFERS.Jokers) do
+            joker = SMODS.Jokers[slug]
+            if joker.order then
+                id = joker.order
+            else
+                i = i + 1
+                id = i + minId
+            end
+            local joker_obj = {
+                discovered = joker.discovered,
+                name = joker.name,
+                set = "Joker",
+                unlocked = joker.unlocked,
+                order = id,
+                key = joker.slug,
+                pos = joker.pos,
+                config = joker.config,
+                rarity = joker.rarity,
+                blueprint_compat = joker.blueprint_compat,
+                eternal_compat = joker.eternal_compat,
+                effect = joker.effect,
+                cost = joker.cost,
+                cost_mult = 1.0,
+                atlas = joker.atlas or nil,
+                mod_name = joker.mod_name,
+                badge_colour = joker.badge_colour,
+                soul_pos = joker.soul_pos,
+                -- * currently unsupported
+                no_pool_flag = joker.no_pool_flag,
+                yes_pool_flag = joker.yes_pool_flag,
+                unlock_condition = joker.unlock_condition,
+                enhancement_gate = joker.enhancement_gate,
+                start_alerted = joker.start_alerted
+            }
+            for _i, sprite in ipairs(SMODS.Sprites) do
+                if sprite.name == joker_obj.key then
+                    joker_obj.atlas = sprite.name
+                end
+            end
+    
+            -- Now we replace the others
+            G.P_CENTERS[slug] = joker_obj
+            if not joker.taken_ownership then
+                table.insert(G.P_CENTER_POOLS['Joker'], joker_obj)
+                table.insert(G.P_JOKER_RARITY_POOLS[joker_obj.rarity], joker_obj)
+            else
+                for kk, v in ipairs(G.P_CENTER_POOLS['Joker']) do
+                    if v.key == slug then G.P_CENTER_POOLS['Joker'][kk] = joker_obj end
+                end
+                if joker_obj.rarity == joker.rarity_original then
+                    for kk, v in ipairs(G.P_JOKER_RARITY_POOLS[joker_obj.rarity]) do
+                        if v.key == slug then G.P_JOKER_RARITY_POOLS[joker_obj.rarity][kk] = joker_obj end
+                    end
+                else
+                    table.insert(G.P_JOKER_RARITY_POOLS[joker_obj.rarity], joker_obj)
+                    local j
+                    for kk, v in ipairs(G.P_JOKER_RARITY_POOLS[joker.rarity_original]) do
+                        if v.key == slug then j = kk end
+                    end
+                    table.remove(G.P_JOKER_RARITY_POOLS[joker.rarity_original], j)
+                end
+            end
+            -- Setup Localize text
+            G.localization.descriptions["Joker"][slug] = joker.loc_txt
+    
+            sendInfoMessage("Registered Joker " .. joker.name .. " with the slug " .. joker.slug .. " at ID " .. id .. ".")
+        end
+    end
+end
--- README.md
+++ README.md
@@ -3,7 +3,4 @@ Aura is a mod that seeks to add animations to every Joker in Balatro.
 
 **12/150** Jokers completed
 
-Known Issues:
-- Jokers don't naturally generate properly when using Steamodded 0.9.8
-
 Artists and progress for each Joker are detailed [here](https://docs.google.com/spreadsheets/d/1MGBtcHmb2hb9bE-ueEiKwMdn3c27fzgU46PYq8b6UhM/edit#gid=0).
